322c2cd448f2a684020716ce2a56a425989e6f7b
Kiien Kyonh Do <dosasha244@gmail.com>
2025-12-09 10:04:05 +0000

    updated

--- dbt/models/mart/mart_green_stadtteil.sql
+++ dbt/models/mart/mart_green_stadtteil.sql
@@ -1,3 +1,5 @@
-select *, 
+select bezirk, stadtteil, cemetry_area, allotment_area, public_garden_area, area_size as area_size_bezirk, 
+    population as population_bezirk, population_density as population_density_per_bezirk, city_districts as city_districts_per_bezirk,
     (cemetry_area + allotment_area + public_garden_area) as green_area
-from {{ref ('prep_green')}}
+from {{ref ('prep_green')}} g
+join {{ref('prep_population_data')}} p using (bezirk)
